d2c45ea3aa0f468eafb33c717cc11c7867126de7
JuliaKrasnoperva <julia.krasnoperova97@gmail.com>
2022-07-03 09:39:06 +0500

    all files have been changed

--- main.js
+++
@@ -1,8 +0,0 @@
-// Анимация меню при клике
-const navIcon = document.querySelector('.nav-icon')
-
-
-navIcon.addEventListener('click', function () {
-    document.querySelector('.nav-icon__middle').classList.toggle('nav-icon-active');
-    document.querySelector(".header-nav-mobile").classList.toggle('header-nav-mobile--active');
-})
